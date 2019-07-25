d5111c712512527462faddcdc76ffc911375e092
Sailarn <denis9524@gmail.com>
2019-07-25 17:24:53 +0300

    - application build

---
+++ client/build/precache-manifest.9aa13924d618965cc5fd78d5e839a9ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba6e805cb38253728773fa4046d4c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7196e60379a1bf08de0e",
+    "url": "/static/css/main.6501441d.chunk.css"
+  },
+  {
+    "revision": "f191fd483f149f80e2af",
+    "url": "/static/js/2.d1e4399f.chunk.js"
+  },
+  {
+    "revision": "7196e60379a1bf08de0e",
+    "url": "/static/js/main.25d7b045.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
